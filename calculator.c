7803d9c8f06521b28df5ecf49a5b337ebb34474c
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-20 18:47:55 -1000

    hw4 setup

---
+++ calculator.c
@@ -0,0 +1,14 @@
+/*
+Write a C program that creates a simple calculator.
+
+Your calculator should be capable of performing basic arithmetic operations such as
+addition, subtraction, multiplication, and division on two user-inputted numbers.
+To accomplish this, utilize the switch-case control structure to determine the chosen
+operation based on the user input. Your code should include proper user prompts, input
+validation, and clear output displaying both the calculation and error messages when
+applicable. 
+Additionally, aim to create a user-friendly and robust calculator program that
+handles various scenarios gracefully, such as division by zero or invalid inputs. 
+
+*/
+
